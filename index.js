8e5496d049c2f9bc649c8d8edf76b16c048dc79b
CyronF <cyronpfraser@gmail.com>
2021-06-08 13:24:08 -0400

    created/edited classes

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-const
+const questions = require ("./utils/questions");
 const inquirer = require ('inquirer');
+const Employee = require ("./lib/Employee");
+const { async } = require("rxjs");
 
+async function main (){
+    const manager = await inquirer.prompt(questions)
+
+    //(new Employee(...Object.values(manager)))
+
+    //create a manager 
+    //ask
+    //generate html string from answers/push to complete team
+    //ask for 
+      //switch
+        //intern
+        //emgineer
+        //done
+    //
+
+}
+
+//start
+main()
 
 
---
+++ utils/questions.js
@@ -0,0 +1,14 @@
+module.exports = [
+{
+  message: "What is your name?",
+  name: "name"
+},
+{
+  message: "What is your id?",
+  name: "id"  
+}, 
+{
+  message: "Wat is your email?",
+  name: "email",
+}
+]
